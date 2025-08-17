a4a7a63044d6d354c667644812b41402af811898
Damir Arh <damir.arh@gmail.com>
2025-08-17 15:06:56 +0200

    Add project files.

---
+++ Cs14ExtensionMembersDisambiguation/BeforeCs14Tests.cs
@@ -0,0 +1,68 @@
+﻿using Shouldly;
+
+namespace Cs14ExtensionMembersDisambiguation.Before;
+
+public static class FirstExtensions
+{
+    public static string Prefix(this string receiver)
+    {
+        return $"First: {receiver}";
+    }
+
+    public static TResult Choose<TResult, TItem>(
+        this IEnumerable<TItem> receiver,
+        TResult first,
+        TResult second
+    )
+    {
+        return first;
+    }
+}
+
+public static class SecondExtensions
+{
+    public static string Prefix(this string receiver)
+    {
+        return $"Second: {receiver}";
+    }
+
+    public static TResult Choose<TResult, TItem>(
+        this IEnumerable<TItem> receiver,
+        TResult first,
+        TResult second
+    )
+    {
+        return second;
+    }
+}
+
+public class BeforeCs14Tests
+{
+    [Test]
+    public void InvokesFirst()
+    {
+        FirstExtensions.Prefix("foo").ShouldBe("First: foo");
+    }
+
+    [Test]
+    public void InvokesSecond()
+    {
+        SecondExtensions.Prefix("foo").ShouldBe("Second: foo");
+    }
+
+    [Test]
+    public void InvokesFirstGeneric()
+    {
+        FirstExtensions
+            .Choose<string, int>(Enumerable.Range(1, 5), "First", "Second")
+            .ShouldBe("First");
+    }
+
+    [Test]
+    public void InvokesSecondGeneric()
+    {
+        SecondExtensions
+            .Choose<string, int>(Enumerable.Range(1, 5), "First", "Second")
+            .ShouldBe("Second");
+    }
+}
---
+++ Cs14ExtensionMembersDisambiguation/WithCs14Tests.cs
@@ -0,0 +1,156 @@
+﻿using Shouldly;
+
+namespace Cs14ExtensionMembersDisambiguation.After;
+
+public static class FirstExtensions
+{
+    extension(string? receiver)
+    {
+        public string? Prefix()
+        {
+            return $"First: {receiver}";
+        }
+
+        public string WithPrefix
+        {
+            get => $"First: {receiver}";
+        }
+
+        public static string Create()
+        {
+            return "First";
+        }
+
+        public static string ExtensionId
+        {
+            get => "First";
+        }
+
+        public static string operator !(string? value)
+        {
+            return $"First: {value}";
+        }
+    }
+
+    extension<TItem>(IEnumerable<TItem> receiver)
+    {
+        public TResult Choose<TResult>(TResult first, TResult second)
+        {
+            return first;
+        }
+    }
+}
+
+public static class SecondExtensions
+{
+    extension(string? receiver)
+    {
+        public string? Prefix()
+        {
+            return $"Second: {receiver}";
+        }
+
+        public string WithPrefix
+        {
+            get => $"Second: {receiver}";
+        }
+
+        public static string Create()
+        {
+            return "Second";
+        }
+
+        public static string ExtensionId
+        {
+            get => "Second";
+        }
+
+        public static string operator !(string? value)
+        {
+            return $"Second: {value}";
+        }
+    }
+
+    extension<TItem>(IEnumerable<TItem> receiver)
+    {
+        public TResult Choose<TResult>(TResult first, TResult second)
+        {
+            return second;
+        }
+    }
+}
+
+public class WithCs14Tests
+{
+    [Test]
+    public void InvokesFirstInstance()
+    {
+        FirstExtensions.Prefix("foo").ShouldBe("First: foo");
+    }
+
+    [Test]
+    public void InvokesSecondInstance()
+    {
+        SecondExtensions.Prefix("foo").ShouldBe("Second: foo");
+    }
+
+    [Test]
+    public void GetsFirstInstance()
+    {
+        FirstExtensions.get_WithPrefix("foo").ShouldBe("First: foo");
+    }
+
+    [Test]
+    public void GetsSecondInstance()
+    {
+        SecondExtensions.get_WithPrefix("foo").ShouldBe("Second: foo");
+    }
+
+    [Test]
+    public void InvokesFirstStatic()
+    {
+        FirstExtensions.Create().ShouldBe("First");
+    }
+
+    [Test]
+    public void InvokesSecondStatic()
+    {
+        SecondExtensions.Create().ShouldBe("Second");
+    }
+
+    [Test]
+    public void GetsFirstStatic()
+    {
+        FirstExtensions.get_ExtensionId().ShouldBe("First");
+    }
+
+    [Test]
+    public void GetsSecondStatic()
+    {
+        SecondExtensions.get_ExtensionId().ShouldBe("Second");
+    }
+
+    [Test]
+    public void InvokesFirstOperator()
+    {
+        FirstExtensions.op_LogicalNot("foo").ShouldBe("First: foo");
+    }
+
+    [Test]
+    public void InvokesSecondOperator()
+    {
+        SecondExtensions.op_LogicalNot("foo").ShouldBe("Second: foo");
+    }
+
+    [Test]
+    public void InvokesFirstGeneric()
+    {
+        FirstExtensions.Choose<int, string>(Enumerable.Range(1, 5), "First", "Second").ShouldBe("First");
+    }
+
+    [Test]
+    public void InvokesSecondGeneric()
+    {
+        SecondExtensions.Choose<int, string>(Enumerable.Range(1, 5), "First", "Second").ShouldBe("Second");
+    }
+}
